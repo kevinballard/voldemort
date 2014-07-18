bad87c6a9a54cd598755d27e6dba80173b77c978
Felix GV <fgv@linkedin.com>
2014-07-18 16:00:02 -0700

    Changed rdb.data.dir to rocksdb.data.dir As suggested by Vinoth
    Added the RocksDB Storage Config in VoldemortConfig's default list.
    
    Added RocksDB's library loading in RocksDbStorageConfiguration
    Fails for me at runtime because of missing dependencies... will need to be revisited.

--- src/java/voldemort/server/VoldemortConfig.java
+++ src/java/voldemort/server/VoldemortConfig.java
@@ -55,6 +55,7 @@ import voldemort.store.readonly.BinarySearchStrategy;
 import voldemort.store.readonly.InterpolationSearchStrategy;
 import voldemort.store.readonly.ReadOnlyStorageConfiguration;
 import voldemort.store.readonly.ReadOnlyStorageEngine;
+import voldemort.store.rocksdb.RocksDbStorageConfiguration;
 import voldemort.store.stats.StatTrackingStore;
 import voldemort.utils.ConfigurationException;
 import voldemort.utils.Props;
@@ -486,7 +487,8 @@ public class VoldemortConfig implements Serializable {
                                                                     MysqlStorageConfiguration.class.getName(),
                                                                     InMemoryStorageConfiguration.class.getName(),
                                                                     CacheStorageConfiguration.class.getName(),
-                                                                    ReadOnlyStorageConfiguration.class.getName()));
+                                                                    ReadOnlyStorageConfiguration.class.getName(),
+                                                                    RocksDbStorageConfiguration.class.getName()));
 
         // start at midnight (0-23)
         this.retentionCleanupFirstStartTimeInHour = props.getInt("retention.cleanup.first.start.hour",
@@ -573,7 +575,7 @@ public class VoldemortConfig implements Serializable {
                                                              10000);
 
         // RocksDB config
-        this.rdbDataDirectory = props.getString("rdb.data.dir", "/tmp/rdb_data_dir");
+        this.rdbDataDirectory = props.getString("rocksdb.data.dir", "/tmp/rdb_data_dir");
 
         validateParams();
     }
@@ -3250,7 +3252,7 @@ public class VoldemortConfig implements Serializable {
    * Where RocksDB should put its data directories
    *
    * <ul>
-   * <li>Property :"rdb.data.dir"</li>
+   * <li>Property :"rocksdb.data.dir"</li>
    * <li>Default : "/tmp/rdb_data_dir"</li>
    * </ul>
 
